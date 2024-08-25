1c88b463276b68c4901fe22b7200eac0c60269d4
Shalom Jnr <shalomjunior007@gmail.com>
2024-08-25 16:03:06 +0100

    aesthetics

--- src/app/components/arrow-down.tsx
+++ src/app/components/arrow-down.tsx
@@ -7,10 +7,10 @@ const ScrollDownArrow = () => {
   return(
     <MdKeyboardDoubleArrowDown
       className='
-        fixed bottom-10 ml-[50%]
+        fixed bottom-5 ml-[50%]
         animate__animated animate__rubberBand animate__slower animate__infinite
         text-2xl cursor-pointer
-      hover:text-[#4169E1] hover:border-[#4169E1] hover:border-[1px] rounded-full hover:bg-white
+      hover:text-[#4169E1] hover:border-[#4169E1] hover:border-[1px] rounded-full
         transition-all duration-500 
       '
       onClick={() => scroll({left: 0, top: 800, behavior: 'smooth'})}
