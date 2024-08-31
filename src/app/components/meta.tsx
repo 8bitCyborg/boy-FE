23c7bdb2b5d14f29e2691b591ebf06a7a3cfeb3b
Shalom Jnr <shalomjunior007@gmail.com>
2024-08-31 19:16:26 +0100

    experience content

--- src/app/components/meta.tsx
+++ src/app/components/meta.tsx
@@ -23,7 +23,8 @@ const Meta = () => {
       </div>
 
       <div className="w-[100vw] md:w-[60vw] bg-black">
-        <div id="about" className="h-[100vh] p-5">
+
+        <div id="about" className="min-h-[100vh] p-5">
           <div className="sticky top-0 p-1 py-2 w-full bg-black">
             <BsPersonBoundingBox className="text-md" />
           </div>
@@ -40,28 +41,48 @@ const Meta = () => {
               Reach out to me, let us work together, and you will be glad you did.
             </p>
             <div className="flex flex-row justify-between my-10 w-[50vw] md:w-[10vw] self-center md:self-end">
-              <a href="https://www.linkedin.com/in/shalom-junior-50661588/" target='_blank' className="text-2xl"><FaLinkedinIn /></a>
-              <a href="mailto:shalombjunior@gmail.com" target='_blank' className="text-2xl"><BiLogoGmail /></a>
-              <a href="https://api.whatsapp.com/send?1=pt_BR&phone=2348143842978" target='_blank' className='text-2xl'><FaWhatsapp /></a>
+              <a href="https://www.linkedin.com/in/shalom-junior-50661588/" target='_blank' className="text-2xl hover:text-blue-600"><FaLinkedinIn /></a>
+              <a href="mailto:shalombjunior@gmail.com" target='_blank' className="text-2xl hover:text-blue-600"><BiLogoGmail /></a>
+              <a href="https://api.whatsapp.com/send?1=pt_BR&phone=2348143842978" target='_blank' className='text-2xl hover:text-blue-600'><FaWhatsapp /></a>
             </div>
           </div>
         </div>
 
-        <div id="experience" className="h-[100vh] p-5">
+        <div id="experience" className="min-h-[100vh] p-5">
           <div className="sticky top-0 p-1 py-2 w-full bg-black">
             <LiaUserClockSolid className="text-md" />
           </div>
-          <div className="flex flex-col justify-center items-end h-full p-1 md:p-10">
+          <div className="flex flex-col justify-start items-end h-full p-1 md:p-10">
             <h3 className="sticky top-2">Experience</h3>
             <span className="h-[1px] w-10 ml-10 bg-white sticky top-8"></span>
-            <p className="mt-10 text-center md:text-right">
-              I am a fullstack Web and Cross Platform Mobile App Developer with about three years of experience. I am familiar with most of the relevant technologies in this field.
-              I have great interpersonal skills as I have proven to be a good communicator who can work with and carry along both technical and  non-technical individuals in any project.
+            <p className="mt-10 text-left">
+              I am a fullstack Web and Cross Platform Mobile App Developer with about three years of experience.
+            </p>
+
+            <p className="mt-10 text-left">
+              I am familiar with most of the relevant web development technologies and continually strive to stay up-to-date with industry standards and best practices. <br/>
+              I primarily work with Javascript, using React/React-Native/NextJS in tandem with HTML and CSS for frontend development.
+            </p>
+
+            <p className="mt-10 text-left">
+              While I admittedly have preference for NestJS and MongoDB for backend development and database management, <br/>
+              I however also have experience with PHP and Laravel, as well as SQL as alternatives.
+            </p>
+
+            <p className="mt-10 text-left">
+              I am familiar with both Redux Toolkit and the ContextAPI for statemanagement.<br/>
+              I am also familiar with both REST and GraphQL for building APIs.
+            </p>
+
+            <p className="mt-10 text-left">
+              Outside of web and mobile app development,
+              I have used Python and C++ and the corresponding libraries and frameworks for Data Analytics and Robotics respectively,
+              although these were mostly for hobbyist projects and not in a professional capacity.
             </p>
           </div>
         </div>
 
-        <div id="projects" className="h-[100vh] p-5">
+        <div id="projects" className="min-h-[100vh] p-5">
           <div className="sticky top-0 p-1 py-2 w-full bg-black">
             <BsBriefcase className="text-md" />
           </div>
